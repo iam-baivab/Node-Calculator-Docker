254d3a4f0c939cad90d9e3832ea0718d2cb8b0de
iam-baivab <baivabbandel@gmail.com>
2024-07-09 23:37:48 +0530

    Updated cal logic

--- public/script.js
+++ public/script.js
@@ -3,17 +3,42 @@ let currentInput = '';
 
 const clearDisplay = () => {
   currentInput = '';
-  display.value = '';
+  display.value = '0';
 };
 
 const inputValue = (value) => {
+  if (value === '.' && currentInput.includes('.')) {
+    return;
+  }
+  
+  if (value === '+/-' && currentInput !== '') {
+    if (currentInput.charAt(0) === '-') {
+      currentInput = currentInput.slice(1);
+    } else {
+      currentInput = '-' + currentInput;
+    }
+    display.value = currentInput;
+    return;
+  }
+
+  if (value === '%') {
+    try {
+      currentInput = eval(currentInput) / 100;
+      display.value = currentInput;
+    } catch (error) {
+      display.value = 'Error';
+    }
+    return;
+  }
+
   currentInput += value;
   display.value = currentInput;
 };
 
 const calculateResult = () => {
   try {
-    currentInput = eval(currentInput);
+    const result = new Function('return ' + currentInput)();
+    currentInput = result.toString();
     display.value = currentInput;
   } catch (error) {
     display.value = 'Error';
